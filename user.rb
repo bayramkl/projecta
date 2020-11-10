739c5311c22c96f5917ecb65c4c115e22c3e7937
bayramkl <mybayram@yahoo.com>
2020-11-10 14:01:24 +0400

    Implement to_json for User class

--- user.rb
+++ user.rb
@@ -10,4 +10,15 @@ class User
   def to_s
     "#{fullname}, #{email}, #{occupation}"
   end
+
+  def to_json
+    "{#{key_val('fullname', fullname)}, #{key_val('email', email)}, #{key_val('occupation', occupation)}}"
+  end
+
+  private
+  def key_val(key, val)
+    "\"#{key}\": \"#{val}\""
+  end
+
+
 end
