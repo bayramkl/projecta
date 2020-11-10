d4c4ac2d4cc0a03b293e9f4dd4c9782e794f6197
andy-mill <69871053+andy-mill@users.noreply.github.com>
2020-11-10 13:24:58 +0400

    Add email to User class

--- user.rb
+++ user.rb
@@ -1,8 +1,9 @@
 class User
-  attr_reader :fullname, :occupation
+  attr_reader :fullname, :occupation, :email
 
-  def initialize(fn, oc)
+  def initialize(fn, oc, em)
     @fullname = fn
     @occupatiion = oc
+    @email = em
   end
 end
